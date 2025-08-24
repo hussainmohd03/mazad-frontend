222c5f9fc7452ea14349cd6bc55e31f610e7e17d
Manaf-10 <manaf.alhujairi2006@gmail.com>
2025-08-24 11:01:22 +0300

    pages and components structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import './App.css'
-
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Home from "./pages/Home";
+import Sell from "./pages/Sell";
+import Watchlist from "./pages/Watchlist";
+import Profile from "./pages/Profile";
+import Activity from "./pages/Activity";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import NavBar from "./components/NavBar";
+import ItemDetails from "./components/ItemDetails";
 const App = () => {
-
   return (
     <>
-      <h1>Test</h1>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/sell" element={<Sell />} />
+        <Route path="/watchlist" element={<Watchlist />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/activity" element={<Activity />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
+      <NavBar />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
