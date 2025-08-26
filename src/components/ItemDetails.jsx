6ad0638bdceae5ecd7ef3f1012dcf78f254fab48
Hussain Ahmed <84622365+hussainmohd03@users.noreply.github.com>
2025-08-26 19:27:06 +0300

    .

--- src/components/ItemDetails.jsx
+++ src/components/ItemDetails.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 const ItemDetails = () => {
   const auctionId = useParams().auctionId;
