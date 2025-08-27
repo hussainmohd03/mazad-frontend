03d05106151d6d8512995c39594a5db6005aacaf
Manaf-10 <manaf.alhujairi2006@gmail.com>
2025-08-27 17:02:45 +0300

    item form 50% done

--- src/pages/Sell.jsx
+++ src/pages/Sell.jsx
@@ -9,40 +9,47 @@ const Sell = () => {
 
   return (
     <div className="sell-page">
-      <header>
-        <p className="activity-header">Sell</p>
-        <div className="toggle-buttons">
-          <button
-            className={activeButton === "on-auction" ? "active" : ""}
-            onClick={() => setActiveButton("on-auction")}
-          >
-            On Auction
-          </button>
-          <button
-            className={activeButton === "sell-item" ? "active" : ""}
-            onClick={() => {
-              setActiveButton("sell-item");
-            }}
-          >
-            Sell Item
-          </button>
-        </div>
-      </header>
-      <main>
-        {activeButton === "on-auction" && (
-          <div className="auctions-grid">
-            {auctions.map((auction) => (
-              <AuctionBox
-                key={auction._id}
-                auction={auction}
-                activeButton={activeButton}
-              />
-            ))}
-          </div>
-        )}
-        {activeButton === "sell-item" && <ItemForm />}
-      </main>
-      <NavBar />
+      {/* {activeButton === "on-auction" && ( */}
+
+      {activeButton === "on-auction" && (
+        <>
+          <header>
+            <p className="activity-header">Sell</p>
+            <div className="toggle-buttons">
+              <button
+                className={activeButton === "on-auction" ? "active" : ""}
+                onClick={() => setActiveButton("on-auction")}
+              >
+                On Auction
+              </button>
+              <button
+                className={activeButton === "sell-item" ? "active" : ""}
+                onClick={() => {
+                  setActiveButton("sell-item");
+                }}
+              >
+                Sell Item
+              </button>
+            </div>
+          </header>
+          <main>
+            <div className="auctions-grid">
+              {auctions.map((auction) => (
+                <AuctionBox
+                  key={auction._id}
+                  auction={auction}
+                  activeButton={activeButton}
+                />
+              ))}
+            </div>
+          </main>
+          <NavBar />
+        </>
+      )}
+
+      {activeButton === "sell-item" && (
+        <ItemForm setActiveButton={setActiveButton} />
+      )}
     </div>
   );
 };
