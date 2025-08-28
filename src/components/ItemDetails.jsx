5e3afa890b29255621f61b5cab89c6ee63b6c33e
Hussain Ahmed <84622365+hussainmohd03@users.noreply.github.com>
2025-08-28 11:38:08 +0300

    remove hardcoded items

--- src/components/ItemDetails.jsx
+++ src/components/ItemDetails.jsx
@@ -1,54 +1,31 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { io } from 'socket.io-client'
 import { useNavigate, useParams } from 'react-router-dom'
+import Client from '../../services/api'
+import { BASE_URL } from '../../globals'
+
 const socket = io('http://localhost:5000')
 
 const ItemDetails = () => {
+  const [auction, setAuction] = useState('')
   const auctionId = useParams().auctionId
   useEffect(() => {
+    const getAuction = async () => {
+      const res = await Client(`${BASE_URL}/auctions/${auctionId}`)
+      setAuction(res.data)
+    }
     socket.emit('joinAuction', auctionId)
 
     socket.on('newBid', (data) => {
       // Update UI with new bid
     })
-
-    socket.on('auctionStatusChanged', (data) => {
-      // Update UI with new status
-    })
-
+    getAuction()
     return () => {
       socket.emit('leaveAuction', auctionId)
       socket.off('newBid')
-      socket.off('auctionStatusChanged')
     }
   }, [auctionId])
-  
-  const auction = {
-    _id: '64f3a2c3d4e5f67890123441',
-    ownerId: '64f1b2c3d4e5f67890123411',
-    startDate: '2025-08-25T10:00:00.000Z',
-    endDate: '2025-08-27T10:00:00.000Z',
-    status: 'upcoming',
-    initialPrice: 2500,
-    currentPrice: 2500,
-    winningBid: null,
-    createdAt: '2025-08-25T09:00:00.000Z',
-    updatedAt: '2025-08-25T09:00:00.000Z',
-    item: {
-      _id: '64f1a2c3d4e5f67890123401',
-      name: 'Luxury Watch',
-      price: 2500,
-      category: 'watches',
-      images: ['items/watch.webp'],
-      approved: true,
-      ownerId: '64f1b2c3d4e5f67890123411',
-      createdAt: '2025-08-18T10:30:00.000Z',
-      updatedAt: '2025-08-20T15:00:00.000Z',
-      description:
-        'Elegant luxury wristwatch with premium craftsmanship, perfect for formal and casual wear.'
-    }
-  }
 
   const getDateFormatted = (dateString) => {
     const formatedDate = new Date(dateString)
@@ -67,19 +44,28 @@ const ItemDetails = () => {
       </div>
       <div className="item-page-body">
         <div className="item-images-container">
-          <img src={`/${auction.item.images}`} alt="item-image" />
+          {/* <img src={`/${auction.item.images}`} alt="item-image" /> */}
+          <img src="/items/watch.webp" alt="item-image" />
         </div>
         <div className="item-details">
-          <p className="grey-1">Lot ID # {auction.item._id}</p>
-          <p className="boldnbig">{auction.item.name}</p>
+          <p className="grey-1">
+            Lot ID # {auction && auction.auction.itemId._id}
+          </p>
+          <p className="boldnbig">{auction && auction.auction.itemId.name}</p>
           <p className="grey-1">Current Bid</p>
-          <p className="boldnbig">BHD {auction.item.price}</p>
+          <p className="boldnbig">
+            BHD {auction && auction.auction.itemId.price}
+          </p>
           <p className="bids-count">
-            0 Bids • Closes on: {getDateFormatted(auction.endDate)}
+            0 Bids • Closes on:{' '}
+            {getDateFormatted(auction && auction.auction.endDate)}
           </p>
           <div className="item-description">
             <p className="description-title">Description</p>
-            <p className='description-text'> {auction.item.description}</p>
+            <p className="description-text">
+              {' '}
+              {auction && auction.auction.itemId.description}
+            </p>
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,24 @@
-import { NavLink } from "react-router-dom";
-import categories from "../objects/categories.json";
-import auctions from "../objects/auctions.json";
-import ItemCard from "../components/ItemCard";
-import CategoryCard from "../components/CategoryCard";
-import NavBar from "../components/NavBar";
+import { NavLink } from 'react-router-dom'
+import categories from '../objects/categories.json'
+import ItemCard from '../components/ItemCard'
+import CategoryCard from '../components/CategoryCard'
+import NavBar from '../components/NavBar'
+import { useEffect, useState } from 'react'
+import Client from '../../services/api'
+import { BASE_URL } from '../../globals'
 const Home = () => {
+  const [auctions, setAuctions] = useState('')
+  useEffect(() => {
+    const getAuctions = async () => {
+      const res = await Client(`${BASE_URL}/auctions?status=ongoing`)
+      console.log(res.data)
+      setAuctions(res.data)
+    }
+    getAuctions()
+  }, [])
   return (
     <>
+    {/* TODO 1: add search functionality */}
       <div className="home-page">
         <div className="home-header">
           <input
@@ -26,16 +38,17 @@ const Home = () => {
             ))}
           </div>
           <div className="items-container">
-            {auctions.map((auction) => (
-              <ItemCard key={auction._id} auction={auction} />
-            ))}
+            {auctions &&
+              auctions.map((auction) => (
+                <ItemCard key={auction._id} auction={auction} />
+              ))}
           </div>
         </div>
       </div>
 
       <NavBar />
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
