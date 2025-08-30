2727acdecb167f90f145046f5e00ec26f4feff08
Manaf <manaf.alhujairi2006@gmail.com>
2025-08-30 16:16:03 +0300

    item form works styling remains

--- src/components/ItemForm.jsx
+++ src/components/ItemForm.jsx
@@ -4,16 +4,32 @@ import Category from "./Category";
 import Price from "./Price";
 import Description from "./Description";
 import Images from "./Images";
-
+import { createItem } from "../../services/item";
 const ItemForm=({ setActiveButton })=> {
   const [activeStep, setActiveStep] = useState(1);
   const [formData, setFormData] = useState({
-    itemName: "",
+    name: "",
     category: "",
     price: 0,
     endDate: null,
     images: [],
   });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const itemData = new FormData();
+    itemData.append("name", formData.name);
+    itemData.append("category", formData.category);
+    itemData.append("price", formData.price);
+    itemData.append("endDate", formData.endDate);
+    formData.images.forEach((image) => {
+      itemData.append("images", image);
+    });
+    console.log(formData);
+    for (let [key, value] of itemData.entries()) {
+  console.log(key, value);
+}
+    createItem(itemData);
+  };
   return (
     <>
       {activeStep === 1 && (
@@ -47,6 +63,7 @@ const ItemForm=({ setActiveButton })=> {
           activeStep={activeStep}
           setFormData={setFormData}
           formData={formData}
+          handleSubmit={handleSubmit}
         />
       )}
     </>
---
+++ src/components/SuccesfulPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SuccesfulPage = () => {
+  return (
+    <div>SuccesfulPage</div>
+  )
+}
+
+export default SuccesfulPage
