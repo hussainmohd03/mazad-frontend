40aa28ad37b6a2c7f26330d19ae1cd0e7d98c112
mad6717 <mad6717@gmail.com>
2025-08-30 18:18:12 +0300

    pie and line char added

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -12,7 +12,8 @@ import {
   ResponsiveContainer,
   PieChart,
   Pie,
-  Cell
+  Cell,
+  Area
 } from 'recharts'
 const backendUrl = import.meta.env.VITE_BACKEND_URL
 
@@ -68,6 +69,7 @@ const AdminDashboard = () => {
     { name: 'Not verified', value: notApprovedUsers }
   ]
   const COLORS = ['#16a34a', '#dc2626']
+
   //  Biddings over time (group by date)
   const biddingData = biddings.reduce((acc, bid) => {
     const date = new Date(bid.createdAt).toLocaleDateString()
@@ -85,11 +87,88 @@ const AdminDashboard = () => {
       <NavBar />
       <div className="Admin-dashboard-container">
         <h2 className="Dashboard-title">Dashboard</h2>
-        <div className="Total-listed-items"></div>
-        <div className="aproved-items"></div>
-        <div className="rejected-items"></div>
-        <div className="total-users"></div>
-        <div className="all-biddings-over-time"></div>
+        <div className="Total-listed-items">
+          <p className="p-in-dashbaard">Statistics</p>
+          <h3>Total Listed Items</h3>
+          <p style={{ fontSize: '24px', fontWeight: '600' }}>{totalListed}</p>
+        </div>
+        <div className="aproved-items">
+          <p className="p-in-dashbaard">Statistics</p>
+          <h3>Approved Items</h3>
+          <p style={{ fontSize: '24px', fontWeight: '600' }}>
+            {approvedListed}
+          </p>
+        </div>
+        <div className="rejected-items">
+          <p className="p-in-dashbaard">Statistics</p>
+          <h3>Rejected Items</h3>
+          <p style={{ fontSize: '24px', fontWeight: '600' }}>
+            {rejectedListed}
+          </p>
+        </div>
+        <div className="total-users">
+          <p className="p-in-dashbaard-user">Statistics</p>
+          <h3>Total Users</h3>
+          <p
+            className="total-users-title"
+            style={{
+              textAlign: 'center',
+              fontSize: '20px',
+              fontWeight: '600'
+            }}
+          >
+            Total Count: {totalUsers}
+          </p>
+          <hr />
+          <ResponsiveContainer width="100%" height="65%" className="pie-chart">
+            <PieChart>
+              <Pie
+                data={userData}
+                dataKey="value"
+                cx="50%"
+                cy="70%"
+                startAngle={180}
+                endAngle={0}
+                innerRadius={60}
+                outerRadius={100}
+                cornerRadius={5}
+                paddingAngle={5}
+              >
+                {userData.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={COLORS[index]} />
+                ))}
+              </Pie>
+              <Tooltip />
+              <Legend verticalAlign="bottom" height={36} />
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="all-biddings-over-time">
+          <p className="p-in-dashbaard-user">Statistics</p>
+          <h3>Biddings Over Time</h3>
+          <ResponsiveContainer width="95%" height="75%">
+            <LineChart data={biddingChartData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="date"
+                tickFormatter={(dateStr) =>
+                  new Date(dateStr).toLocaleString('en-UK', { month: 'short' })
+                }
+              />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+
+              <Line
+                type="monotone"
+                dataKey="count"
+                stroke="#2563eb"
+                strokeWidth={2}
+                fillOpacity={0.9}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
       </div>
     </>
   )
