9849cfcc6cae3082320fd3951879370594a81fb5
Manaf-10 <manaf.alhujairi2006@gmail.com>
2025-09-02 14:55:31 +0300

    watchlist works both ways (thank god)

--- src/components/ItemDetails.jsx
+++ src/components/ItemDetails.jsx
@@ -82,8 +82,9 @@ const ItemDetails = () => {
     getAuction();
     const fetchWatchList = async () => {
       const response = await Client.get("/watchlist/me");
+      console.log(response.data);
       setIsInWatchList(
-        response.data.some((item) => item.auctionId === auctionId)
+        response.data.some((item) => item.auctionId._id === auctionId)
       );
     };
     fetchWatchList();
